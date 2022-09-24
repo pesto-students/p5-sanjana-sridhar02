29cbabfd21e98ea5065922d2beb54b2d32c15d6e
sridhar02 <kattasridhar02@gmail.com>
2022-09-24 17:29:29 +0530

    ✨ init

---
+++ Week-6/Excerise-6.2.js
@@ -0,0 +1,27 @@
+/**
+ * Problem 6.2 Spiral Order Matrix II
+ *
+ * Problem Description: Given a matrix of m * n elements (m rows, n columns), return all elements of the matrix in spiral order.
+ *
+ * Example: Given the following matrix: [ [ 1, 2, 3 ], [ 4, 5, 6 ], [ 7, 8, 9 ] ]
+ * You should return[1, 2, 3, 6, 9, 8, 7, 4, 5]
+ */
+
+const spiralOrderMatrix = (arr) => {
+  const result = [];
+
+  return result;
+};
+
+const input = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+spiralOrderMatrix(input);
+
+// Space Complexity :
+// Time Complexity:
+
+// https://jsoncrack.com/
