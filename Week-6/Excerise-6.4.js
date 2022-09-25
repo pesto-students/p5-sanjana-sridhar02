ea559e12cd715623eb24901012affc1a054f27f0
sridhar02 <kattasridhar02@gmail.com>
2022-09-26 01:18:34 +0530

    solution 6.4

--- Week-6/Excerise-6.4.js
+++ Week-6/Excerise-6.4.js
@@ -1,27 +1,61 @@
 /**
- * Problem 6.2 Spiral Order Matrix II
+ * Problem 6.4 : Best time to buy and sell stock
  *
- * Problem Description: Given a matrix of m * n elements (m rows, n columns), return all elements of the matrix in spiral order.
+ * You are given an array prices where prices[i] is the price of a given stock on the ith day.
  *
- * Example: Given the following matrix: [ [ 1, 2, 3 ], [ 4, 5, 6 ], [ 7, 8, 9 ] ]
- * You should return[1, 2, 3, 6, 9, 8, 7, 4, 5]
+ * You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+ *
+ * Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+ *
+ * Example 1: Input: prices = [7,1,5,3,6,4] Output: 5
+ *
+ * Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5. Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+ *
+ *  Example 2: Input: prices =[7,6,4,3,1] Output: 0
+ *
+ * Explanation: In this case, no transactions are done and the maxprofit = 0 Constraints: 1 <= prices.length <= 105 0 <= prices[i] <= 104
  */
 
-const spiralOrderMatrix = (arr) => {
-  const result = [];
+// we use two pointer's method
 
-  return result;
+const maxProfit = (prices) => {
+  let sellIdx = prices.length - 1;
+  let profit = 0;
+  for (let buyIdx = prices.length - 1; buyIdx >= 0; buyIdx--) {
+    let buyVal = prices[buyIdx];
+    let sellVal = prices[sellIdx];
+    console.log({ buyIdx, sellIdx, buyVal, sellVal });
+    if (buyVal - sellVal >= 0) {
+      sellIdx = buyIdx;
+    } else {
+      let price = sellVal - buyVal;
+      profit = Math.max(profit, price);
+
+      console.log({ price, profit });
+    }
+  }
+
+  return profit;
 };
 
-const input = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-];
+console.log(maxProfit([7, 1, 5, 3, 6, 4]));
 
-spiralOrderMatrix(input);
+// Time complexity: O(n)
+// Space complexity : O(1)
 
-// Space Complexity :
-// Time Complexity:
+const maxProfit2 = (prices) => {
+  let profit = 0;
 
-// https://jsoncrack.com/
+  for (let i = 0; i < prices.length - 1; i++) {
+    for (let j = i + 1; j < prices.length - 1; j++) {
+      const currentProfit = prices[j];
+      if (currentProfit > profit) {
+        profit = currentProfit;
+      }
+    }
+  }
+
+  return profit;
+};
+// Time complexity: O(n2) n square so we don't consider this
+// Space complexity : O(1)
